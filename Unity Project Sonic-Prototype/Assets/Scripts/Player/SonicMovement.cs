927e55622d598a4d930d6ddea39033a2f6598048
JonnyTheKing-3 <jonathangrcrdrgz@gmail.com>
2025-01-28 21:33:01 -0800

    Fixed it! Slope movement works. Time to polish it as well as apply rotation to the GFX
    Four vertical velocity, which was our Vector3.Project, I only add it to our velocity if the player is not grounded, which would be to simply subtract our y velocity by gravity. Because this new method adds gravity, and does so in a much simpler way, I no longer have to use a constant force component to simulate gravity when the player wasn't grounded.
    Because vertical velocity was only applied when in the air, and our horizontal velocity already moves our velocity towards our desired velocity, we can still move on upwards
    However, this solution caused a problem. When the player would jump, sometimes, the player abruplty stops rising right after pressing the jump. This indicated that when the player jumped, the player tried to stick itself to the ground and would apply gravity. To fix this, we simply make sure that not only would we need to check if the character is grounded, but also if they are ready to jump (this check goes with sticking the player to the ground AND when considering vertical velocity). readyToJump is a bool that is only true for a fraction of a second right after the player jumps. This is done so that the ground checking ray doesn't accidently mark the player as grounded right after jumping. It gives the player enough time to reach a big enough height so that the ray can't accidently mark the player as grounded for the first few frames after leaving the ground. By adding this check, we make sure the player reaches enough height were the system won't accidently mark the player as grounded, and therefore, apply gravity/stick player to the ground.
    Now that that is solved, time to polish the slope movement a bit more. I need the player to rotate towards the normal of the surface as well as shoot the grounding ray  towards the surface the player is on, rather than just straight down.

--- Unity Project Sonic-Prototype/Assets/Scripts/Player/SonicMovement.cs
+++ Unity Project Sonic-Prototype/Assets/Scripts/Player/SonicMovement.cs
@@ -29,48 +29,36 @@ public class SonicMovement : MonoBehaviour
     public float playerHeight;
     public LayerMask whatIsGround;
     public RaycastHit surfaceHit;
-    public float threshold;
 
     [Header("STATUS")]
     public bool grounded;
     public float horizontalInput;
     public float verticalInput;
-    public Vector3 moveDirection;
+    private Vector3 moveDirection;
 
     [Header("REFERENCES")]
     public Transform orientation;
     public Rigidbody rb;
-    public ConstantForce cf;
     private Quaternion defaultRotation;
 
     [Header("DEBUG RAYS")] 
-    [SerializeField] private bool FixedUpdateRay;
-    [SerializeField] private bool UpdateRay;
-    [SerializeField] private bool targetVelocityRay;
-    [SerializeField] private bool ProjectRay;
-    [SerializeField] private bool ProjectOnPlaneRay;
-
+    [SerializeField] private bool Ray;
 
     private void Start()
     {
         // getting references
         rb = GetComponent<Rigidbody>();
-        cf = GetComponent<ConstantForce>();
 
         // initiating values
         readyToJump = true;
-        cf.enabled = true;
-        cf.force = Vector3.down * gravity;
-        cf.enabled = false;
         defaultRotation = transform.rotation;
     }
     
     private void Update()
     {
         grounded = Physics.Raycast(transform.position, -transform.up, out surfaceHit, playerHeight, whatIsGround);
-        StickPlayerToGround();
-        ApplyGravity();
         MyInput();
+        StickPlayerToGround();
     }
 
     private void MyInput()
@@ -88,43 +76,10 @@ public class SonicMovement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Keypad1))
         {
-            FixedUpdateRay = !FixedUpdateRay;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad2))
-        {  
-            UpdateRay = !UpdateRay;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad3))
-        {  
-            targetVelocityRay = !targetVelocityRay;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad4))
-        {  
-            ProjectRay = !ProjectRay;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad5))
-        {  
-            ProjectOnPlaneRay = !ProjectOnPlaneRay;
+            Ray = !Ray;
         }
     }
-    
-    private void ApplyGravity()
-    {
-        cf.enabled = !grounded;
-        
-        // Update the gravity in case we change it during play
-        if (cf.enabled) { cf.force = Vector3.down * gravity; }
-    }
-    
-    private void StickPlayerToGround()
-    {
-        if (!grounded) {return;}
 
-        Vector3 targetPosition = new Vector3(transform.position.x, surfaceHit.point.y + playerHeight -.1f, transform.position.z);
-        transform.position = targetPosition;
-        
-    }
-    
     private void Jump()
     {
         // reset y velocity
@@ -137,6 +92,15 @@ public class SonicMovement : MonoBehaviour
         readyToJump = true;
     }
     
+    private void StickPlayerToGround()
+    {
+        // Only stick player to the ground when the player is on the ground
+        if (!grounded || !readyToJump) {return;}
+
+        Vector3 targetPosition = new Vector3(transform.position.x, surfaceHit.point.y + playerHeight -.1f, transform.position.z);
+        transform.position = targetPosition;
+    }
+    
     private void FixedUpdate()
     {
         MovePlayer();
@@ -147,20 +111,32 @@ public class SonicMovement : MonoBehaviour
         // Calculate move direction based on input and camera orientation
         moveDirection = (orientation.forward * verticalInput + orientation.right * horizontalInput).normalized;
 
-        Vector3 Surface = grounded ? surfaceHit.normal : transform.up;
-        // Get the correct force to apply according to the surface to player is in
+        // Get surface normal based on grounding status
+        Vector3 Surface = grounded ? surfaceHit.normal : Vector3.up;
+    
+        // Apply movement on the slope by projecting onto the surface
         Vector3 SurfaceAppliedDirection = Vector3.ProjectOnPlane(moveDirection, Surface);
 
+        // Calculate target velocity
         Vector3 targetVelocity = SurfaceAppliedDirection.normalized * speed;
-        
-        // preparing specs for moving the character. Turn speed is to make sure when massively changing directions, the player loses speed
+
+        // Smoothly rotate towards target velocity and apply acceleration or deceleration
         float rad = turnSpeed * Mathf.PI * Time.deltaTime;
         float appropriateAcceleration = moveDirection != Vector3.zero ? acceleration : deceleration;
-     
-        // move character
-        rb.velocity += Vector3.RotateTowards(Vector3.ProjectOnPlane(rb.velocity, Surface), targetVelocity, rad,
-                    appropriateAcceleration * Time.deltaTime) + Vector3.Project(rb.velocity, Surface);
         
-        if (ProjectRay) {Debug.DrawRay(transform.position, Vector3.Project(rb.velocity, Surface), Color.blue);}
+        // Move our current velocity towards our desired velocity
+        Vector3 horizontalVelocity = Vector3.RotateTowards(Vector3.ProjectOnPlane(rb.velocity, Surface), targetVelocity, rad,
+            appropriateAcceleration * Time.deltaTime);
+
+        // Preserve vertical velocity
+        float verticalVelocity = rb.velocity.y;
+
+        // If grounded, reset vertical velocity. Otherwise, apply gravity to it
+        if (grounded && readyToJump) { verticalVelocity = 0f; }
+        else { verticalVelocity += -gravity * Time.fixedDeltaTime;}
+
+        // Combine horizontal and vertical velocity
+        rb.velocity = horizontalVelocity + Vector3.up * verticalVelocity;
     }
+
 }
